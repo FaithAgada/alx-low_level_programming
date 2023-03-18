feeab5755787629b79b0c794203e64fe72df9d65
FaithAgada <faith.eagada@gmail.com>
2023-03-18 05:44:58 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,20 +13,17 @@ int main(void)
 	srand(0));
 	n = rand() - Rand_MAX / 2;
 
-	if (n >0)
+	if (n > 0)
 	{
 
 		printf("%d is positive\n", n);
-	
 	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
 	else
-	{
 		printf("%d is negative\n", n);
-	
-        return (0);
-}	
-		
+	return (0);
+
+}
