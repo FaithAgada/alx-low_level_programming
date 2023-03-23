5b6c8af8dad7d34359802dc2e5bc350e7af60554
FaithAgada <faith.eagada@gmail.com>
2023-03-22 17:56:01 -0700

    2-alphabet.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,17 @@
 
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter:
+	char c;
+	int i = 0;
 
-	while (count++ <= 9)
+	while (i <= 9)
 	{
-		for (letter = 'a': letter <= 'z'; letter++)
-			_putcher(letter);
+		for (c = 'a'; c <= 'z'; c++)
+		{
+		_putchar(c);
+		}
 		_putchar('/n');
+		i++;
 	}
+
+}
