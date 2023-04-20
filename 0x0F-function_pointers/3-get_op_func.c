842b64af8e232bf315cb617bd809988fe0308900
FaithAgada <faith.eagada@gmail.com>
2023-04-19 22:12:51 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,12 +17,17 @@ int (*get_op_func(char *s))(int, int)
 			{"/", op_div},
 			{"%", op_mod},
 			{NULL, NULL},
-		};
+			};
 
-		int i = 0;
+			int i = 0;
 
-		while (ops[i].op != NULL && *(ops[i].op) != *s)
-			i++;
+			while (i < 5)
+			{
+				if (s && s[0] == ops[i].op[o] && !s[1])
+					return (ops[i].f);
+				i++;
 
-		return (ops[i].f);
+			}
+
+			return (NULL);
 }
