62bffeae17e906e11fced4be16c17e4bbcb0b406
FaithAgada <faith.eagada@gmail.com>
2023-05-03 16:59:20 -0700

    0-prints

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @nest: points to the following node
+ *
+ * Description: singly linked list node structure
+ * An ALX Project
+ */
+typedef struct listint_s
+{
+        int n;
+        struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int sum_listint(listint_t *head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int pop_listint(listint_t **head);
+void free_listint2(listint_t **head);
